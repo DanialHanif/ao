2b981e898ea248ab6780c55cca6aa9c2f983bb8c
DanialHanif <danialhanif@outlook.com>
2020-09-22 05:59:46 +0800

    Update package,json, update.json and url.js

--- src/url.js
+++ src/url.js
@@ -2,14 +2,13 @@
 
 module.exports = {
   app: 'https://todo.microsoft.com/?app',
-  community: 'https://gitter.im/klaussinani/ao',
-  homepage: 'https://klaussinani.github.io/ao',
-  issue: 'https://github.com/klaussinani/ao/issues/new',
-  keyboardShortcutsRef: 'https://github.com/klaussinani/ao#keyboard-shortcuts',
-  license: 'https://github.com/klaussinani/ao/blob/master/license.md',
-  release: 'https://github.com/klaussinani/ao/releases/latest',
-  search: 'https://github.com/search?q=+is:issue+repo:klaussinani/ao',
-  searchFeatureRequests: 'https://github.com/klaussinani/ao/labels/feature-request',
-  source: 'https://github.com/klaussinani/ao',
-  update: 'https://raw.githubusercontent.com/klaussinani/ao/master/docs/update.json'
+  homepage: 'https://danialhanif.github.io/ao-reborn',
+  issue: 'https://github.com/danialhanif/ao-reborn/issues/new',
+  keyboardShortcutsRef: 'https://github.com/danialhanif/ao-reborn#keyboard-shortcuts',
+  license: 'https://github.com/danialhanif/ao-reborn/blob/master/license.md',
+  release: 'https://github.com/danialhanif/ao-reborn/releases/latest',
+  search: 'https://github.com/search?q=+is:issue+repo:danialhanif/ao-reborn',
+  searchFeatureRequests: 'https://github.com/danialhanif/ao-reborn/labels/feature-request',
+  source: 'https://github.com/danialhanif/ao-reborn',
+  update: 'https://raw.githubusercontent.com/danialhanif/ao-reborn/master/docs/update.json'
 };
